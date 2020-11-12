46c69aff0e98ca0b83178237d5c5c55d9f5b1e31
Maria Doyle <maria.doyle@petermac.org>
2020-11-12 13:01:05 +1100

    Change BBC to Peter Mac

--- R/finalise_plot.R
+++ R/finalise_plot.R
@@ -15,27 +15,24 @@ left_align <- function(plot_name, pieces){
   return(grob)
 }
 
-create_footer <- function (source_name, logo_image_path) {
+create_footer <- function (source_name) {
   #Make the footer
   footer <- grid::grobTree(grid::linesGrob(x = grid::unit(c(0, 1), "npc"), y = grid::unit(1.1, "npc")),
                            grid::textGrob(source_name,
-                                          x = 0.004, hjust = 0, gp = grid::gpar(fontsize=16)),
-                           grid::rasterGrob(png::readPNG(logo_image_path), x = 0.944))
+                                          x = 0.004, hjust = 0, gp = grid::gpar(fontsize=16)))
   return(footer)
 
 }
 
-#' Arrange alignment and save BBC ggplot chart
+#' Arrange alignment and save Peter Mac ggplot chart
 #'
-#' Running this function will save your plot with the correct guidelines for publication for a BBC News graphic.
-#' It will left align your title, subtitle and source, add the BBC blocks at the bottom right and save it to your specified location.
+#' Running this function will save your plot with the correct guidelines for publication for a Peter Mac graphic.
+#' It will left align your title, subtitle and source and save it to your specified location.
 #' @param plot_name The variable name of the plot you have created that you want to format and save
 #' @param source_name The text you want to come after the text 'Source:' in the bottom left hand side of your side
 #' @param save_filepath Exact filepath that you want the plot to be saved to
 #' @param width_pixels Width in pixels that you want to save your chart to - defaults to 640
 #' @param height_pixels Height in pixels that you want to save your chart to - defaults to 450
-#' @param logo_image_path File path for the logo image you want to use in the right hand side of your chart,
-#'  which needs to be a PNG file - defaults to BBC blocks image that sits within the data folder of your package
 #' @return (Invisibly) an updated ggplot object.
 
 #' @keywords finalise_plot
@@ -44,8 +41,7 @@ create_footer <- function (source_name, logo_image_path) {
 #' source = "The source for my data",
 #' save_filepath = "filename_that_my_plot_should_be_saved_to-nc.png",
 #' width_pixels = 640,
-#' height_pixels = 450,
-#' logo_image_path = "logo_image_filepath.png"
+#' height_pixels = 450
 #' )
 #'
 #' @export
@@ -53,10 +49,9 @@ finalise_plot <- function(plot_name,
                           source_name,
                           save_filepath=file.path(Sys.getenv("TMPDIR"), "tmp-nc.png"),
                           width_pixels=640,
-                          height_pixels=450,
-                          logo_image_path = file.path(system.file("data", package = 'bbplot'),"placeholder.png")) {
+                          height_pixels=450) {
 
-  footer <- create_footer(source_name, logo_image_path)
+  footer <- create_footer(source_name)
 
   #Draw your left-aligned grid
   plot_left_aligned <- left_align(plot_name, c("subtitle", "title", "caption"))
